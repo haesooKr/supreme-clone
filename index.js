9fc9a127876cec6b69f9e9f34f6c989fbe052612
haesooKr <gotn676@gmail.com>
2020-02-29 20:32:50 -0500

    Add getCurrentTime for NYC

---
+++ index.js
@@ -0,0 +1,25 @@
+const timeHTML = document.querySelector('#js-time');
+
+function getCurrentTime(){
+  let current = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  let toArr = current.split(' ');
+  let [month, date, year] = toArr[0].substr(0, toArr[0].length - 1).split('/');
+  let [hour, minutes] = toArr[1].split(':');
+  let mid = toArr[2].toLowerCase();
+  
+  [month, date, hour, minutes] = [month, date, hour, minutes].map(makeTwoDigits);
+  
+  let format = `${month}/${date}/${year} ${hour}:${minutes}${mid} NYC`
+  console.log(format);
+  timeHTML.innerText = format;
+}
+
+getCurrentTime();
+
+function makeTwoDigits(num) {
+  if(num.length < 2){
+    return '0'+num;
+  } else {
+    return num;
+  }
+}
